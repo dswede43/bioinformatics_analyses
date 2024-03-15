d6a667e22b675cae81d08d8ae38740a27cb5db0c
Daniel Stratis <dstratis@protonmail.com>
2024-03-15 11:57:09 -0600

    My custom functions

---
+++ Functions/append_gene_names.R
@@ -0,0 +1,25 @@
+#Append gene names
+#---
+#Function to append gene annotations to a data frame containing ensembl id's
+
+#paramters
+#gene_names: list of gene annotations (HGNC, entrez, GENCODE, etc.) with associated ensembl id's as the list names
+#ensembl_df: data frame containing ensembl id's as a column (column must be names 'ensembl')
+
+#load packages
+library(dplyr)
+
+
+#Function
+#---
+append_gene_names = function(gene_names, ensembl_df){
+	
+	#convert the gene names to a data frame
+	gene_names_df = data.frame(ensembl = names(gene_names), gene_name = gene_names)
+	rownames(gene_names_df) = NULL
+	
+	#join these genes names to the data frame containing ensembl id's
+	result = inner_join(ensembl_df, gene_names_df, by = 'ensembl')
+	
+	return(result)
+}

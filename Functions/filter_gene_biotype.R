8f3be1009e08c49f9b66ce5cd93f15fcc429b197
Daniel Stratis <dstratis@protonmail.com>
2024-04-08 16:19:36 -0600

    function to filter a data frame by gene biotypes

---
+++ Functions/filter_gene_biotype.R
@@ -0,0 +1,29 @@
+#Filter gene biotype
+#---
+#Function to filter a data frame containing gene ensembl id's to a specific gene biotype
+
+#paramters
+#gene_biotypes: list of gene biotypes (protein-coding, lncRNA, snRNA, etc.) with associated ensembl id's as the list names
+#ensembl_df: data frame containing ensembl id's as a column (column must be named 'ensembl')
+
+
+#load packages
+library(dplyr)
+
+
+#Function
+#---
+filter_gene_biotype = function(gene_biotypes, ensembl_df, biotype = 'protein_coding'){
+	
+	#convert the gene names to a data frame
+	gene_biotypes_df = data.frame(ensembl = names(gene_biotypes), gene_biotype = gene_biotypes)
+	rownames(gene_biotypes_df) = NULL
+	
+	#join these genes names to the data frame containing ensembl id's
+	result = left_join(ensembl_df, gene_biotypes_df, by = 'ensembl')
+	
+	#remove non-protein coding biotypes
+	result = filter(result, gene_biotype == biotype)
+	
+	return(result)
+}

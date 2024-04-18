8e2231052c573760aed64fd409b400057c13bc99
Daniel Stratis <dstratis@protonmail.com>
2024-04-18 13:48:01 -0600

    Batch downloading FASTQ files

--- Data/Fastq_batch_downloading.sh
+++ Data/Fastq_batch_downloading.sh
@@ -2,34 +2,41 @@
 
 #Batch downloading FASTQ files
 #---
-#This script automates batch downloading of FASTQ files using the SRA toolkit
-#(https://github.com/ncbi/sra-tools/wiki)
+#This script automates the batch downloading of SRA files and extraction
+#into FASTQ files containing paired-end reads using the SRA toolkit.
+#SRA-toolkits documentation: https://github.com/ncbi/sra-tools/wiki
 
-#define SRA sample id's
+#define global variables
+DIR="/path/to/directory" #working directory
+
+#define the range of SRA sample id's
 SRA_MIN=6298258
-SRA_MAX=6298384
+SRA_MAX=6298259
 
-SRA_IDS=()
+
+#Download FASTQ files
+#---
+#create a list of SRA sample id's
+sra_ids=()
 for ((i = SRA_MIN; i <= SRA_MAX; i++)); do
-    SRA_IDS+=("SSR$i")
+    sra_ids+=("SSR$i")
 done
 
-#download sample .sra files
-for SRA_ID in ${SRA_IDS[@]}; do
-    echo "Currently downloading: $SRA_ID"
-    prefetch $SRA_ID
+#download sample SRA files
+for sra_id in ${sra_ids[@]}; do
+    echo "Currently downloading: $sra_id"
+    prefetch --progress "$DIR/samples/$sra_id"
 done
 
-#convert .sra files into .fastq
+#convert SRA files into FASTQ files
 #--split-3:
 #splits paired-end reads into *_1.fastq and *_2.fastq files, unmated reads are placed into *.fastq
 #--skip-technical:
 #ignores technical reads (adapters, primers, barcodes, etc)
-for SRA_ID in ${SRA_ID[@]}; do
+for sra_id in ${sra_id[@]}; do
     echo "Generating fastq for: $SRA_ID"
-    fasterq-dump $SRA_ID --split-3 --skip-technical --outdir $SRA_ID
-    
-    #remove the .sra file
-    rm -rfv $SRA_ID/*.sra
-done
+    fasterq-dump "$DIR/samples/$SRA_ID" --split-3 --skip-technical --outdir "$DIR/samples/$sra_id"
 
+    #remove the .sra file
+    rm -rfv "$DIR/samples/$SRA_ID/*.sra"
+done
